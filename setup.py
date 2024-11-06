a98cbdb59896b1533f2c987ec32ebfef7b8dfb65
Athira <athirakbiju955@gmil.com>
2024-11-06 16:25:57 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Cohort1-Dexian',
+    author_email= 'athirakbiju955@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
